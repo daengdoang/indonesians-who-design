95efd26c2d67f4b39cb3c5b1b2ffe772112cfa70
Daeng Muhammad Feisal <daengdoang@gmail.com>
2022-08-03 00:00:23 +0700

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -192,7 +192,7 @@ export default function Home({ designers }) {
           <p>
             If you’ve been added to the directory and would like to opt-out or
             make an edit to your profile, please send us a message at
-            daengmfeisal[at]gmail[dot]com or DM via twitter @daengdoang.
+            hello[at]dae[dot]ng or DM via twitter <a href="https://twitter.com/daengdoang"> </a>@daengdoang</a>.
           </p>
           <h3>Why is my nomination taking so long to show?</h3>
           <p>
